c8357de49030ac36351640745b9d5c623a6d5cc5
Alessander <alessander.logistica@gmail.com>
2020-05-25 17:48:09 -0300

    create a repository to interact with Tutorials from the database

---
+++ src/main/java/com/example/app/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.app.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
