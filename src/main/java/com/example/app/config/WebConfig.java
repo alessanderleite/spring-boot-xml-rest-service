930bde136bd32555ece000807c5211b673953190
Alessander <alessander.logistica@gmail.com>
2020-05-26 00:23:59 -0300

    final update

--- src/main/java/com/example/app/config/WebConfig.java
+++ src/main/java/com/example/app/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
 		configurer.defaultContentType(MediaType.APPLICATION_XML);
 	}
 }
